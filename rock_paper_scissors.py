176463f90236135af6d896c4fd4cac530acf9719
Justin Huynh <justinhuynh639@gmail.com>
2023-09-24 00:08:19 -0700

    added random generator

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,8 +3,11 @@ import random
 #set initial user and computer scores
 user_wins = 0
 computer_wins = 0
+#variable for rps
+options = ["rock", "paper", "scissors"]
+options[]
 
-#input rock, paper, scissors
+#input rock, paper, scissors (rps)
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     #if user chooses to quit
@@ -12,7 +15,11 @@ while True:
         break
     #check if user types rock, paper, or scissors in list
     #check if user types something valid within the list
-    if user_input not in ["rock", "paper", "scissors"]:
+    if user_input not in options:
         continue
+
+    #generate random number for rps
+    #rock: 0, paper: 1, scissors: 2
+    random_number = random.randint(0, 2)
 
 print("goodbye")
