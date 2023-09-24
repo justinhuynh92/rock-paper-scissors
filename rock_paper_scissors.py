5e0ab47503283e4033c821ea35e7c47f61a5c3e9
Justin Huynh <justinhuynh639@gmail.com>
2023-09-24 00:28:53 -0700

    completed rock paper scissors game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,7 +6,7 @@ computer_wins = 0
 #variable for rps
 options = ["rock", "paper", "scissors"]
 
-#input rock, paper, scissors (rps)
+#input rock, paper, scissors (rps) and make lowercase
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     #if user chooses to quit
@@ -23,9 +23,25 @@ while True:
     #index to access string from options
     computer_pick = options[random_number]
     print("Computer picked", computer_pick + ".")
+
     #if user wins, add 1 to their score
     if user_input == "rock" and computer_pick == "scissors":
         print("You won!")
         user_wins += 1
 
-print("goodbye")
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+    #add to computer's score if it wins
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+#print when broken out of the while loop
+print("You won", user_wins, "times.")
+print("Computer won", computer_wins, "times.")
+print("Goodbye!")
